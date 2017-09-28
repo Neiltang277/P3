20d1ceda8be40d5dbaa44508225ff721d578a436
neiltang277 <neiltang277@gmail.com>
2017-09-28 17:44:22 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,156 @@
+from datetime import datetime
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import xml.etree.ElementTree as ET  # Use cElementTree or lxml if too slow
+
+# OSM_FILE = "data/san-francisco_california.osm"  # Replace this with your osm file
+# SAMPLE_FILE = "data/san-francisco_california_sample.osm"
+OSM_FILE = "data/hangzhou_china.osm"  # Replace this with your osm file
+SAMPLE_FILE = "data/hangzhou_china_sample.osm"
+SAMPLE_SF_FILE = "data/san-francisco_california_sample.osm"
+
+k = 1000 # Parameter: take every k-th top level element
+
+class MongoConnect(object):
+    """docstring for MongoConnect."""
+    def __init__(self):
+        super(MongoConnect, self).__init__()
+        self.client = MongoClient("mongodb://myapi:abc1234@127.0.0.1/apiDB")
+        self.db = self.client['apiDB']
+
+    def get_one(self):
+        ''' 查询一条数据 '''
+        return self.db.sanfrancisco.find_one()
+
+    def add_one(self,item):
+        ''' 新增数据 '''
+        return self.db.sanfrancisco.insert_one(item)
+
+    # data overview
+    def count_all(self):
+        ''' 查询一条数据 '''
+        return self.db.sanfrancisco.count()
+
+    def count_by_type(self,type):
+        return self.db.sanfrancisco.find({'type':type}).count()
+
+    def count_unique_users(self):
+        return self.db.sanfrancisco.distinct('created.user').length
+
+    def count_unique_users(self):
+        return self.db.sanfrancisco.aggregate([{"$group":{"_id":"$created.user", "count":{"$sum":1}}},
+        {"$sort":{"count":1}}, {"$limit":1}])
+
+
+
+class GetSample(object):
+    """docstring for GetSample."""
+    def __init__(self):
+        super(GetSample, self).__init__()
+
+    def get_element(self,OSM_FILE, tags=('node', 'way', 'relation')):
+        context = iter(ET.iterparse(OSM_FILE, events=('start', 'end')))
+        _, root = next(context)
+        for event, elem in context:
+            if event == 'end' and elem.tag in tags:
+                yield elem
+                root.clear()
+
+    def save_sample(self):
+        print(SAMPLE_FILE)
+        with open(SAMPLE_FILE, 'wb') as output:
+            output.write(bytes('<?xml version="1.0" encoding="UTF-8"?>\n',encoding='utf-8'))
+            output.write(bytes('<osm>\n  ',encoding='utf-8'))
+
+            # Write every kth top level element
+            for i, element in enumerate(self.get_element(OSM_FILE)):
+                if i % k == 0:
+                    output.write(ET.tostring(element, encoding='utf-8'))
+            output.write(bytes('</osm>',encoding='utf-8'))
+
+    def to_json(self,i, element):
+
+        # if i % k == 0:
+        # 处理node/way/realations等标签属性
+        data = {'type':element.tag}
+        value =element.attrib
+        temp = {}
+        pos = [0,0]
+        for key in value:
+            if key == 'lat':
+                pos[0]= value[key]
+            elif key == 'lon':
+                pos[1]= value[key]
+            else :
+                temp[key] = value[key]
+        data['created'] = temp
+        if pos!= [0,0]:
+            data['pos'] = pos
+
+        # 处理标签内二级标签
+        for item in element.iter("tag"):
+            # print(item.tag)
+            if item.tag == 'tag':
+                kv = item.attrib
+                # print(item.attrib)
+                # 是否是: 分割字符串
+                isColon = self.is_colon(kv['k'])
+                if isColon:
+                    if isColon[0] in data:
+                        if type(data[isColon[0]])== type('string'):
+                            # print(data[isColon[0]])
+                            data[isColon[0]] = {
+                                'default': data[isColon[0]],
+                                isColon[1] : kv['v']
+                            }
+                        else:
+                            temp= data[isColon[0]].copy()
+                            temp[isColon[1]] = kv['v']
+                            print(temp)
+                            data[isColon[0]] = temp
+                    else:
+                        data[isColon[0]] = {
+                            isColon[1] : kv['v']
+                        }
+                else:
+                    data[kv['k']] = kv['v']
+
+        # print(data)
+        return data
+        data = {}
+
+    def is_colon(self, testiment):
+        rest = testiment.split(':')
+        if len(rest) > 1:
+            return rest
+        else:
+            return False
+
+
+    # def commbine_colon_string(self, target):
+    #     target
+
+
+
+def main():
+    # print('hello')
+    obj = MongoConnect()
+    rest  = obj.get_one()
+    # print(rest)
+
+    getSample = GetSample()
+
+    # getSample.save_sample()
+
+    for i, element in enumerate(getSample.get_element(SAMPLE_SF_FILE)):
+        item = getSample.to_json(i, element)
+        # if i == 7:
+        obj.add_one(item)
+        # print('----')
+    print(obj.count_all())
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,139 @@
+# OpenStreetMap Project
+# Data Wrangling with MongoDB
+### TANG Haoxiang
+
+**Map Area: San Francisco, United States**
+
+https://www.openstreetmap.org/relation/111968
+https://mapzen.com/data/metro-extracts/metro/san-francisco_california/
+
+1. Problems Encountered in the Map
+    Over­abbreviated Street Names
+    Postal Codes
+2. Data Overview
+3. Additional Ideas
+    Contributor statistics and gamification suggestion
+    Additional data exploration using MongoDB
+    Conclusion
+
+## 0. 明确项目目的
+
+1) 将osm内数据进行筛选，清洗，最终导入到数据库内。
+
+
+## 1. Problems Encountered in the Map
+1. python2 和python3的语法差异
+1. dic.has_key()
+可以替换成 if key in distinct
+
+2. 关于子数组添加的问题
+
+
+
+数据导入步骤
+1. 了解osm数据结构
+osm: 跟标签
+
+node:
+    tag: 对于该坐标点的描述, k为类型, v为值
+
+way: 路的范围
+    nd: 路的节点, ref属性为参考
+    tag: 路的属性, k为类型, v为值
+
+relations: 关联区域
+    member: 子节点，
+    tag: 区域属性
+
+2. 数据录入
+导入数据库的格式样例：
+```
+{
+"id": "2406124091",
+"type: "node",
+"visible":"true",
+"created": {
+          "version":"2",
+          "changeset":"17206049",
+          "timestamp":"2013-08-03T16:43:42Z",
+          "user":"linuxUser16",
+          "uid":"1219059"
+        },
+"pos": [41.9757030, -87.6921867],
+"address": {
+          "housenumber": "5157",
+          "postcode": "60625",
+          "street": "North Lincoln Ave"
+        },
+"amenity": "restaurant",
+"cuisine": "mexican",
+"name": "La Cabana De Don Luis",
+"phone": "1 (773)-271-5176"
+}
+```
+
+3. 录入过程中出现的问题
+1) name标签处理问题
+1) 英文名 多个, 用/粉哥
+2) 不同语种的名字
+
+json二级字段的处理问题
+重复复制, 判断
+
+2)tiger标签, gnis标签
+经过查询，tiger标签是一种早起由美国tiger数据库导入的标签，属于历史遗留标签，但是在07年之后逐渐在去除tiger标签的使用，但是在San Francisco数据库中依然存在多处使用tiger标签，
+
+County FIPS code
+
+More than one county in the US has a FIPS code of "001".
+
+3) county标签
+Santa Clara, CA
+
+<tag k="addr:city" v="San Francisco" />
+<tag k="addr:state" v="CA" />
+
+<tag k="addr:state" v="ca" />
+
+
+2. Data Overview
+**File sizes**
+san-francisco_california.osm ...... 1.41 GB
+
+###  Number of documents
+```
+> db.char.find().count()
+1555851
+```
+### Number of nodes
+```
+> db.char.find({"type":"node"}).count()
+1471349
+```
+### Number of wayss
+```
+> db.char.find({"type":"way"}).count()
+84502
+```
+### Number of unique users
+```
+> db.char.distinct({"created.user"}).length
+336
+```
+### Top 1 contributing user
+```
+> db.char.aggregate([{"$group":{"_id":"$created.user", "count":{"$sum":1}}},
+{"$sort":{"count":1}}, {"$limit":1}])
+[ { "_id" : "jumbanho", "count" : 823324 } ]
+```
+
+3. Additional Ideas
+
+
+## Conclusion
+
+
+---
+reference:
+
+http://wiki.openstreetmap.org/wiki/Key:amenity
